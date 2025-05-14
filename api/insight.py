669782e58864a40cdd227604c57100b4b6d343fd
Shreevatsa PU <121449115+unusualmold2003@users.noreply.github.com>
2025-05-14 19:08:32 +0530

    Update insight.py

--- api/insight.py
+++ api/insight.py
@@ -9,7 +9,7 @@ import google.generativeai as genai
 
 # Load environment
 load_dotenv()
-genai.configure(api_key='INSERT_API_KEY_HERE')
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 app = FastAPI()
 
